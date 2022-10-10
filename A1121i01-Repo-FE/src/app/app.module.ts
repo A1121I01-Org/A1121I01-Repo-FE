d600bfda0742881ebc07a953a26890bd35cccd06
ntrunghieu <trunghieu270302@gmail.com>
2022-10-10 16:03:59 +0700

    HieuNT commit customer-list-delete

--- A1121i01-Repo-FE/src/app/app.module.ts
+++ A1121i01-Repo-FE/src/app/app.module.ts
@@ -11,6 +11,10 @@ import {SecurityModule} from './security/security.module';
 import {MaterialModule} from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { ErrorComponent } from './error/error.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ListCustomerComponent} from './customer/list-customer/list-customer.component';
+
 
 
 @NgModule({
@@ -27,7 +31,8 @@ import { ErrorComponent } from './error/error.component';
     CartModule,
     AccountModule,
     SecurityModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
