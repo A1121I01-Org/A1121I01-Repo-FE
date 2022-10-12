36f9b26801c462fbfce9be5aa659561a418762fc
lehungson0510 <hungsonle0510@gmail.com>
2022-10-13 04:01:15 +0700

    update css html

--- A1121i01-Repo-FE/src/app/employee/employee-routing.module.ts
+++ A1121i01-Repo-FE/src/app/employee/employee-routing.module.ts
@@ -14,9 +14,6 @@ const routes: Routes = [
   },
   {
     path: 'employee/edit/:id', component: EditEmployeeComponent
-  },
-  {
-    path: 'employee/update/password/:id', component: EditEmployeeComponent
   }
 ];
 
