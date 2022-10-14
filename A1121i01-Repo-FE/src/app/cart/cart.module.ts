cd0407a24cbbc8d181a32f53c06507a11eb9a1a3
nguyenvansy1 <ashewarakill2@gmail.com>
2022-10-14 11:38:11 +0700

    Done 99%

--- A1121i01-Repo-FE/src/app/cart/cart.module.ts
+++ A1121i01-Repo-FE/src/app/cart/cart.module.ts
@@ -5,13 +5,20 @@ import { CartRoutingModule } from './cart-routing.module';
 import { DetailCartComponent } from './detail-cart/detail-cart.component';
 import { SellCartComponent } from './sell-cart/sell-cart.component';
 import { SellExportComponent } from './sell-export/sell-export.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NotifierModule} from 'angular-notifier';
 
 
 @NgModule({
   declarations: [DetailCartComponent, SellCartComponent, SellExportComponent],
-  imports: [
-    CommonModule,
-    CartRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        CartRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        NotifierModule,
+    ]
 })
 export class CartModule { }
--- A1121i01-Repo-FE/src/app/model/cart/icart.ts
+++ A1121i01-Repo-FE/src/app/model/cart/icart.ts
@@ -1,7 +1,12 @@
+import {ICustomer} from '../customer/icustomer';
+import {ICartStatus} from './icart-status';
+
 export interface ICart {
   cartId?: number;
   cartCode?: string;
   cartQuantity?: number;
   cartDateCreate?: string;
   cartTotalMoney?: number;
+  cartCustomerId?: ICustomer;
+  cartStatusId?: ICartStatus;
 }
