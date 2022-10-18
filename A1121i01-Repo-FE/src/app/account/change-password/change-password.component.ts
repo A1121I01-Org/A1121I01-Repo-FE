3b01f68ebe201583ddd934a2805460568d99f050
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-10-18 22:33:29 +0700

    1033PM 18102022 catching error status 412

--- A1121i01-Repo-FE/src/app/account/change-password/change-password.component.ts
+++ A1121i01-Repo-FE/src/app/account/change-password/change-password.component.ts
@@ -15,6 +15,7 @@ export class ChangePasswordComponent implements OnInit {
   passwordForm: FormGroup;
   accountId: number;
   updatePassword: Password = {};
+  error = false;
 
   constructor(private accountService: AccountServiceService, private fb: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute) {
   }
@@ -43,6 +44,9 @@ export class ChangePasswordComponent implements OnInit {
         (error) => {
           if (error.status === 500) {
             this.router.navigateByUrl('/auth/access-denied');
+          }
+          if (error.status === 412) {
+            this.error = true;
           }
         },
         () => {
